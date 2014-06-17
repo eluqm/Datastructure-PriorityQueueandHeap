1ab0bd70d004f9cb18e8d5f8e590dd8cc6f5e412
root <root@localhost.localdomain>
2014-06-17 05:59:47 -0500

    projetoeclipse

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,22 @@
+
+/**
+ * 					TIPOS E ESTRUTURAS DE DADOS (SCC-5763)
+							TRABALHO – 2014
+						Prof. Ricardo J. G. B. Campello
+	
+	nome(s): 
+	
+	Paul Bustios Belizario			 nº: 	
+	Edson Francisco Luque Mamani     nº: 9057024
+ 
+ */
+
+
+
+	
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 
 public class Test {
 	
